58fb4cb4887a673efc665d3f5125bb119002f98f
Adis Bacic <adisbacic96@hotmail.com>
2024-01-07 20:51:01 +0100

    Changed from introvideo to img

--- src/components/Misc/BenefitsCard.js
+++ src/components/Misc/BenefitsCard.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import placeholderImage from '../../assets/images/elementor-placeholder-image.webp';
-import mainwindwGuardia from '../../assets/images/mainwindowGuardia.png';
+import Ghome from '../../assets/images/GuardiaHome.png';
 import IntroVideo from './IntroVideo';
+
 
 const BenefitsCard = () => {
     return (
@@ -10,13 +11,13 @@ const BenefitsCard = () => {
             className="flex mt-20 mb-20 flex-col items-center rounded-lg bg-gray-800  w-full md:max-w-full"
         >
             <div className="flex flex-col md:flex-row">
-                {/* <img
+                <img
                     className="rounded w-full md:w-1/2 h-96 md:h-auto"
-                    src={mainwindwGuardia}
+                    src={Ghome}
                     alt=""
-                /> */}
+                />
 
-                <IntroVideo />
+                {/* <IntroVideo /> */}
 
 
                 <div className="flex flex-col justify-start p-4 leading-normal w-full md:w-1/2">
