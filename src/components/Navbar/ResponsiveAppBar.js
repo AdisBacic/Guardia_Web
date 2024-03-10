2971e6381099f6c921d016c4e4d96664d9097061
Adis Bacic <adisbacic96@hotmail.com>
2024-03-10 15:46:28 +0100

    navbarfix

--- src/components/Navbar/ResponsiveAppBar.js
+++ src/components/Navbar/ResponsiveAppBar.js
@@ -63,7 +63,7 @@ const Navbar = () => {
       </style>
 
       <nav className="fixed w-full z-20 top-0 start-0 bg-[#090A0F]">
-        <div className="flex flex-wrap md:flex-row mx-auto p-4 justify-between max-w-[1200px]">
+        <div className="flex flex-wrap md:flex-row mx-auto  justify-between max-w-[1200px]">
           <div
             className="flex items-right space-x-0 cursor-pointer"
             onClick={navigateHome}
@@ -72,11 +72,11 @@ const Navbar = () => {
               xmlns="http://www.w3.org/2000/svg"
               fill="#77d47e"
               viewBox="0 0 21 18"
-              className="w-8 h-8 pl-1"
+              className="w-8 h-8 pl-0 mt-1 pb-1 pb-1"
             >
               <path d="M5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.560c1.11.300 2.229.655 2.887.870a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.990a11.775 11.775 0 0 1-2.517 2.453a7.159 7.159 0 0 1-1.048.625c-.280.132-.581.240-.829.240s-.548-.108-.829-.240a7.158 7.158 0 0 1-1.048-.625a11.777 11.777 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692a1.54 1.54 0 0 1 1.044-1.262a62.456 62.456 0 0 1 2.185-.870z" />
             </svg>
-            <span className="text-xl font-semibold whitespace-nowrap text-white px-1 font-poppins-light">
+            <span className="text-xl font-semibold whitespace-nowrap text-white px-1 mt-2 font-poppins-light">
               Guardia
             </span>
           </div>
@@ -107,7 +107,7 @@ const Navbar = () => {
           </div>
           <div
             className={`flex-grow ${isMenuOpen ? "flex" : "hidden"} md:flex ${
-              isMenuOpen ? "justify-start" : "justify-end"
+              isMenuOpen ? "justify-end" : "justify-end"
             }`}
             id="navbar-sticky"
           >
