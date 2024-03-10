2c7dd561a4450ef54f648f5bd778760ef9a60ddd
Adis Bacic <adisbacic96@hotmail.com>
2024-03-10 13:22:11 +0100

    design changes new animated underline

--- src/components/Navbar/ResponsiveAppBar.js
+++ src/components/Navbar/ResponsiveAppBar.js
@@ -38,34 +38,42 @@ const Navbar = () => {
   return (<>
 
     <style>
-    {`
-      .underline-animation::after {
-        content: '';
-        display: block;
-        width: 0;
-        height: 2px;
-        background: white;
-        transition: width .3s;
-      }
-      .underline-animation:hover::after {
-        width: 100%;
-      }
-    `}
-  </style>
+      {`
+          .nav-link {
+            color: #fff;
+            position: relative;
+            text-decoration: none;
+          }
+          .nav-link::before {
+            content: "";
+            position: absolute;
+            width: 100%;
+            height: 2px; /* Adjusted for visibility */
+            background-color: white;
+            bottom: 0;
+            left: 0;
+            transform-origin: right;
+            transform: scaleX(0);
+            transition: transform 0.2s ease-in-out;
+          }
+          .nav-link:hover::before, .nav-link:focus::before {
+            transform-origin: left;
+            transform: scaleX(1);
+          }
+        `}
+    </style>
 
 
-
-    
     <nav className="fixed w-full z-20 top-0 start-0 bg-[#090A0F]">
       <div className="flex flex-wrap md:flex-row mx-auto p-4 justify-between">
         <div
           className="flex items-right space-x-0 cursor-pointer"
           onClick={navigateHome}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="#77d47e" viewBox="0 0 21 18" className='w-7 h-7 pl-1'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="#77d47e" viewBox="0 0 21 18" className='w-8 h-8 pl-1'>
             <path d="M5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.560c1.11.300 2.229.655 2.887.870a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.990a11.775 11.775 0 0 1-2.517 2.453a7.159 7.159 0 0 1-1.048.625c-.280.132-.581.240-.829.240s-.548-.108-.829-.240a7.158 7.158 0 0 1-1.048-.625a11.777 11.777 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692a1.54 1.54 0 0 1 1.044-1.262a62.456 62.456 0 0 1 2.185-.870z" />
           </svg>
-          <span className="text-xl font-semibold whitespace-nowrap text-white px-1">Guardia</span>
+          <span className="text-2xl font-semibold whitespace-nowrap text-white px-1">Guardia</span>
         </div>
 
         <div className="md:hidden" style={{ position: 'fixed', right: 10 }}>
@@ -91,7 +99,8 @@ const Navbar = () => {
                     } else {
                       navigate(page.path);
                     }
-                  }} className="block py-2 px-5 text-white rounded  md:hover:bg-transparent md:border-0 underline-animation"
+                  }} className="block py-2 px-5 text-white rounded hover:bg-transparent md:border-0 nav-link"
+
 
                 >
                   {page.name}
@@ -102,7 +111,7 @@ const Navbar = () => {
         </div>
       </div>
     </nav>
-    </>
+  </>
   );
 };
 
