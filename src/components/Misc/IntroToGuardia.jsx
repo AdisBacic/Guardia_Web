379f54a4a1787b34beabbf4b932e1d76d37c882a
Kristian Jeremic <kristianjer@gmail.com>
2024-03-10 15:46:58 +0100

    Update settings

--- src/components/Misc/IntroToGuardia.jsx
+++ src/components/Misc/IntroToGuardia.jsx
@@ -6,11 +6,11 @@ import { NavLink } from "react-router-dom";
 
 const IntroToGuardia = () => {
   return (
-    <Container className="top-1/4 relative">
+    <Container className="top-1/4 relative  rounded-xl">
       <div className="flex h-[50vh]">
         <div className="text-white w-full lg:w-2/3 ">
           <h1 className="text-4xl font-poppins-medium pb-8">
-            Guardia the easiest way to be GDPR compliant, Revoutionalize how you
+            <span className="bg-gradient-to-r from-green-600 to-green-900 bg-clip-text text-transparent">Guardia</span> the easiest way to be GDPR compliant, Revoutionalize how you
             dhtin
           </h1>
           <p className="text-gray-400 font-poppins-light pb-8">
@@ -25,7 +25,7 @@ const IntroToGuardia = () => {
             <Button variant="outlined">Läs mer</Button>
           </NavLink>
         </div>
-        <img src={shieldImg} alt="Guardia" className="hidden lg:block absolute -right-40 -top-20 h-4/5" />
+        <img src={shieldImg} alt="Guardia" className="hidden lg:block absolute -right-40 -bottom-40 h-full" />
       </div>
     </Container>
   );
