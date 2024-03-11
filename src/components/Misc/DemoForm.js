0058e8897e6ace8bf8e534ee23d57a23855122f5
Kristian Jeremic <kristianjer@gmail.com>
2024-03-11 13:09:52 +0100

    Add padding

--- src/components/Misc/DemoForm.js
+++ src/components/Misc/DemoForm.js
@@ -56,7 +56,7 @@ const DemoForm = () => {
     return (
         <Box className="max-w-xs mx-auto p-4 sm:p-8 bg-gradient-to-r from-gray-700 to--300 text-white border border-gray-900 rounded-xl sm:max-w-lg shadow-silver">
             <Typography variant="h5" component="h2" gutterBottom className="text-center mb-8 text-white font-semibold">
-                Boka ett personligt demo
+                Boka personligt demo
             </Typography>
             <form onSubmit={handleSubmit}>
 
--- src/components/Misc/IntroToGuardia.jsx
+++ src/components/Misc/IntroToGuardia.jsx
@@ -35,43 +35,59 @@ const IntroToGuardia = () => {
 
   return (
     <>
-    {days === 0 && hours === 0 && minutes === 0 ? (
-      <div className="flex justify-center items-center mt-20">
-        <div className="text-center p-4  rounded-lg bg-[#090A0F]">
-          <span className="text-xl font-bold text-white-500">The offer has ended.</span>
+      {days === 0 && hours === 0 && minutes === 0 ? (
+        <div className="flex justify-center items-center mt-20">
+          <div className="text-center p-4  rounded-lg bg-[#090A0F]">
+            <span className="text-xl font-bold text-white-500">
+              The offer has ended.
+            </span>
+          </div>
         </div>
-      </div>
-    ) : (
-      <div className="flex justify-center items-center mt-20 mb-5">
-        <div className="bg-[#090A0F] rounded-lg p-4 shadow-lg">
-        <p className="bg-gradient-to-r from-blue-500 to-blue-700 bg-clip-text text-transparent text-2xl mt-2 mb-4 text-center">Lanseringserbjudande upp till 50% rabatt</p>
+      ) : (
+        <div className="flex justify-center items-center mt-20 mb-5">
+          <div className="bg-[#090A0F] rounded-lg p-4 shadow-lg">
+            <p className="bg-gradient-to-r from-blue-500 to-blue-700 bg-clip-text text-transparent text-2xl mt-2 mb-4 text-center">
+              Lanseringserbjudande upp till 50% rabatt
+            </p>
 
-          <div className="flex items-center justify-between space-x-2">
-            <div className="text-center">
-              <p className="text-white-500 text-lg font-mono font-bold">{String(days).padStart(2, '0')}</p>
-              <span className="text-white-400 text-xs uppercase">Dagar</span>
-            </div>
-            <span className="text-white-500 font-bold">:</span>
-            <div className="text-center">
-              <p className="text-white-500 text-lg font-mono font-bold">{String(hours).padStart(2, '0')}</p>
-              <span className="text-white-400 text-xs uppercase">Timmar</span>
-            </div>
-            <span className="text-white-500 font-bold">:</span>
-            <div className="text-center">
-              <p className="text-white-500 text-lg font-mono font-bold">{String(minutes).padStart(2, '0')}</p>
-              <span className="text-white-400 text-xs uppercase">Minuter</span>
-            </div>
-            <span className="text-white-500 font-bold">:</span>
-            <div className="text-center">
-              <p className="text-white-500 text-lg font-mono font-bold">{String(seconds).padStart(2, '0')}</p>
-              <span className="text-white-400 text-xs uppercase">Sekunder</span>
+            <div className="flex items-center justify-between space-x-2">
+              <div className="text-center">
+                <p className="text-white-500 text-lg font-mono font-bold">
+                  {String(days).padStart(2, "0")}
+                </p>
+                <span className="text-white-400 text-xs uppercase">Dagar</span>
+              </div>
+              <span className="text-white-500 font-bold">:</span>
+              <div className="text-center">
+                <p className="text-white-500 text-lg font-mono font-bold">
+                  {String(hours).padStart(2, "0")}
+                </p>
+                <span className="text-white-400 text-xs uppercase">Timmar</span>
+              </div>
+              <span className="text-white-500 font-bold">:</span>
+              <div className="text-center">
+                <p className="text-white-500 text-lg font-mono font-bold">
+                  {String(minutes).padStart(2, "0")}
+                </p>
+                <span className="text-white-400 text-xs uppercase">
+                  Minuter
+                </span>
+              </div>
+              <span className="text-white-500 font-bold">:</span>
+              <div className="text-center">
+                <p className="text-white-500 text-lg font-mono font-bold">
+                  {String(seconds).padStart(2, "0")}
+                </p>
+                <span className="text-white-400 text-xs uppercase">
+                  Sekunder
+                </span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    )}
+      )}
 
-      <Container className="mt-10 relative rounded-xl pb-10">
+      <Container className="mt-10 relative rounded-xl pb-10 border">
         <div className="flex h-[50vh]">
           <div className="text-white w-full lg:w-4/5">
             <h1 className="text-4xl font-poppins-medium pb-8">
@@ -82,27 +98,33 @@ const IntroToGuardia = () => {
               , förändra ditt sätt att hantera data idag!
             </h1>
             <p className="text-gray-400 font-poppins-light pb-8">
-              Simpel och effektiv hantering av nycklar och värdefulla tillgångar. Kämpar ditt företag med pappersarbete, att följa GDPR-förordningar och rutin i det vardagliga arbetet? 
-              Guardia erbjuder en enkel webblösning eller app för att administrera utlåning och inlämning av artiklar. Automatisera processen och följ GDPR-förordningar med enkelhet. <br />
+              Simpel och effektiv hantering av nycklar och värdefulla
+              tillgångar. Kämpar ditt företag med pappersarbete, att följa
+              GDPR-förordningar och rutin i det vardagliga arbetet? Guardia
+              erbjuder en enkel webblösning eller app för att administrera
+              utlåning och inlämning av artiklar. Automatisera processen och
+              följ GDPR-förordningar med enkelhet. <br />
               Det ska vara enkelt att göra rätt.
             </p>
-
-            <NavLink to="/demo" className="pr-2">
-              <Button
-                variant="contained"
-                className="bg-gradient-to-r from-green-600 to-green-900 rounded-none"
-              >
-                Boka ett demo
-              </Button>
-            </NavLink>
-            <NavLink to="/moreinfo">
-              <Button variant="outlined">Läs mer</Button>
-            </NavLink>
+            <div className="pb-20">
+              <NavLink to="/demo" className="pr-2">
+                <Button
+                  variant="contained"
+                  className="bg-gradient-to-r from-green-600 to-green-900 rounded-none"
+                >
+                  Boka Demo
+                </Button>
+              </NavLink>
+              <NavLink to="/moreinfo">
+                <Button variant="outlined">Läs mer</Button>
+              </NavLink>
+            </div>
           </div>
+
           <img
             src={shieldImg}
             alt="Guardia"
-            className="hidden lg:block absolute -right-40 -top-5 h-3/5 "
+            className="hidden lg:block absolute -right-40 -top-5 h-96 "
           />
         </div>
       </Container>
